eb1ac7ae4219096ca780c369692ebf91090bfe0c
lraeusch <l.raeuschel@uni-muenster.de>
2026-04-28 13:05:30 +0200

    Deploy

--- README.md
+++
@@ -1,5 +0,0 @@
-# Emmas_Pommesblog
-
-Code kommt in lib/main
-
-Code runnen "flutter run -d chrome" im ordner emmas_pommes_blog
--- emmas_pommes_blog/android/app/src/main/kotlin/com/example/emmas_pommes_blog/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.emmas_pommes_blog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
